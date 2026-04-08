f1882200f064c8e20da41ebae87875dc720a49a9
DmitriiProshutinskii <koptehe@gmail.com>
2026-04-08 17:57:58 +0300

    Main screen with buttons

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search = findViewById<MaterialButton>(R.id.search_button)
+        val media = findViewById<MaterialButton>(R.id.media_button)
+
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        search.setOnClickListener(searchClickListener)
+
+        media.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на Медиа!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.settings_button -> {
+                Toast.makeText(this, "Нажали на настройки!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
