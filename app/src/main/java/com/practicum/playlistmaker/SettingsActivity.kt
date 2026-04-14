057d528b7230345f20d01f29e3bd77b23d9499e1
DmitriiProshutinskii <koptehe@gmail.com>
2026-04-14 17:05:08 +0300

    Implement settings intents

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +24,29 @@ class SettingsActivity : AppCompatActivity() {
         val backButton = findViewById<MaterialToolbar>(R.id.settings_toolbar)
         backButton.setOnClickListener {
             finish()
+        }
+
+        findViewById<MaterialTextView>(R.id.share_app_button).setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_url))
+            }
+            startActivity(Intent.createChooser(shareIntent, null))
+        }
+
+        findViewById<MaterialTextView>(R.id.support_button).setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.support_body))
+            }
+            startActivity(supportIntent)
+        }
+
+        findViewById<MaterialTextView>(R.id.terms_button).setOnClickListener {
+            val termsIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.terms_of_use_url)))
+            startActivity(termsIntent)
         }
     }
 }
