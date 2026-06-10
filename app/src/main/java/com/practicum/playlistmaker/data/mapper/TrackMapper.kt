5385696c42d907990408d34b7e712e6a5465e0c0
DmitriiProshutinskii <koptehe@gmail.com>
2026-06-10 17:03:12 +0300

    Fix mapper for Track for search history

--- app/src/main/java/com/practicum/playlistmaker/data/mapper/TrackMapper.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/mapper/TrackMapper.kt
@@ -2,10 +2,9 @@ package com.practicum.playlistmaker.data.mapper
 
 import com.practicum.playlistmaker.data.network.dto.TrackDto
 import com.practicum.playlistmaker.domain.model.Track
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 fun TrackDto.toDomain(): Track = Track(
+    trackId = trackId,
     trackName = trackName,
     artistName = artistName,
     trackTimeMillis = trackTimeMillis,
